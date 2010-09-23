73c238dd4e7ad16ac246aa115899be73ba6e4f1e
Lin Jen-Shin <godfat@godfat.org>
2010-09-24 04:10:45 +0800

    remove unused

--- Minei.scala
+++ Minei.scala
@@ -58,7 +58,6 @@ case class Imp(val map_raw: Array[Array[Int]]){
   }
 
   case class ClueSet() extends TreeSet[Clue]{
-    def test(result: Choices): Choices = result
     def conclude: Clue = Clue(map(_.amount).min, overlap)
     def overlap: List[Pos] = poses.tail.foldRight(poses.head)(_.intersect(_))
     def poses: List[List[Pos]] = map(_.poses).toList
