35a1acdac9209ac6ec043e7e585731e1a10c01eb
Lin Jen-Shin <godfat@godfat.org>
2010-09-23 13:06:37 +0800

    we might have more classes

--- godfat.sh
+++ godfat.sh
@@ -3,5 +3,5 @@
 scalac -classpath war/WEB-INF/classes:../gwt-2.0.4/gwt-servlet.jar Godfat.scala
 
 mkdir -p war/WEB-INF/classes/org/godfat/mine/
-mv org/godfat/mine/Godfat.class war/WEB-INF/classes/org/godfat/mine/
+mv org/godfat/mine/*.class war/WEB-INF/classes/org/godfat/mine/
 rm -r org
