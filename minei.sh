36b7fbb33ca30dea2c0ffb9478010a3631a16962
Lin Jen-Shin <godfat@godfat.org>
2010-09-24 23:02:50 +0800

    now favor ant to build the jar

--- minei.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-scalac -classpath war/WEB-INF/classes:../gwt-2.0.4/gwt-servlet.jar Minei.scala
-
-mkdir -p war/WEB-INF/classes/org/godfat/minei/
-mv org/godfat/minei/*.class war/WEB-INF/classes/org/godfat/minei/
-rm -r org
