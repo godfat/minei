3319b5dbd2a5997ec6c5fae13aec202b85581c06
Lin Jen-Shin <godfat@godfat.org>
2010-09-28 01:48:28 +0800

    move commented test to test.scala

---
+++ test.scala
@@ -0,0 +1,32 @@
+// case study:
+// 01110
+// 0?x?0
+// 01110
+//
+// 00?1?
+// 113x?
+// 1x?x?
+// 11211
+
+// --00-
+// x-?3-
+// x4?xx
+// x-x--
+
+// 2???
+// 22??
+// -1x?
+// 11??
+
+// val imp = Imp(Array(Array(0,1), Array(2,3)))
+// println(imp.map)
+// val c0 = imp.Clue(1, List((0,0)))
+// val c1 = imp.Clue(2, List((0,0)))
+// val c2 = imp.Clue(2, List((0,1)))
+// val c3 = imp.Clue(2, List((1,0)))
+// println(c0.compare(c0)) //  0
+// println(c0.compare(c1)) // -1
+// println(c0.compare(c2)) // -1
+// println(c2.compare(c1)) //  1
+// println(c1.compare(c2)) // -1
+// println(c2.compare(c3)) // -1
